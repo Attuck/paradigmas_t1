e2744b22deacedd8b88c7511394de3b976af188e
Oscar Corella Quiros <b32080@ecci.ucr.ac.cr>
2017-09-08 19:09:29 -0600

    todo lo que apu no pudo hacer

--- tarea1.pl
+++ tarea1.pl
@@ -8,18 +8,14 @@
 % Óscar Corella
 %
 % ------------------------------------------------------------------------------
-% listap(+Objeto): verdadero si Objeto es una lista, falso si no
-%    Objeto: cualquier objeto Prolog
-%    ?- listap(a).
-%    ?- listap([]).
-%    ?- listap([a,b,c]).
-listap([]) :- !.
-listap([X|Y]) :- listap(Y).
 % 1. Predicados para el procesamiento de árboles y conjuntos
 %
 % bpp(+N,+A,-S): S es el subárbol de A cuya raíz es N; S debe ser nil si el subárbol no existe
 %
-bpp(N,[],S):- S = nil, !.
-bpp(N,[N|T],S):- format('~w ',[N]), S =[N|T], !.
-bpp(N,[H|T],S):- listap(H), bpp(N,H,S),!.
-bpp(N,[H|T],S):- format('~w ',[H]), bpp(N,T,S), !.
+%bpp(N,[],S)!.
+
+
+bpp(N,[N|T],S):- format('~w ',[N]), S = [N|T], !.
+bpp(N,[H|T],S):- is_list(H), length(H, L), L > 0, bpp(N,H,S),!.
+bpp(N,[H|T],S):- is_list(H), bpp(N,T,S), !.
+bpp(N,[H|T],S):- not(is_list(H)), format('~w ',[H]), bpp(N,T,S), !.
